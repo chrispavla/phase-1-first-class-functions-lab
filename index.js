2dc78b5226e978fbadda5faeb89dcb6a0067eb4b
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-18 18:18:05 -0400

    commit tree bbcb2023ace8ead83b373f2a8893047a4d3ecbf9

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+// const returnFirstTwoDrivers = function(drivers) {
+//     return drivers.slice(0,2)
+// }
+
+// const returnLastTwoDrivers = function(drivers) {
+//     return drivers.slice(2,4)
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// function createFareMultiplier(num) {
+//     return function(number) {
+//         return number * num
+//     }
+// }
+
+// const fareDoubler = createFareMultiplier(2)
+
+// const fareTripler = createFareMultiplier(3)
+
+// function selectDifferentDrivers(drivers, func) {
+//     return func(drivers)
+// }
+
+// console.log(fareDoubler(2))
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(2, arr.length);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (num) {
+    return int * num;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, func) {
+  return func(arr);
+}
